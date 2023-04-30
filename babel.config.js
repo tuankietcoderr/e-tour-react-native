8a7e3be2b6abb818e78e48e41f515170b6708aed
Tuan Kiet Tran <charlieismylife2003@gmail.com>
2023-04-30 15:40:09 +0700

    Config eas and prettier

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,24 @@
-module.exports = function(api) {
+const tsconfig = require("./tsconfig.json");
+let rawAlias = tsconfig.compilerOptions.paths;
+let alias = {};
+
+for (let x in rawAlias) {
+  alias[x.replace("/*", "")] = rawAlias[x].map((p) => {
+    return p.replace("/*", "");
+  })[0];
+}
+
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo", "@babel/preset-typescript"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias,
+        },
+      ],
+    ],
   };
 };
